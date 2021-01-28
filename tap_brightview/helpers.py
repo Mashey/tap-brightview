fa348eb435c8f7c81e3642f9534c8eba0f20dd7c
Jordan <jordan.wallace.williams@gmail.com>
2021-01-27 23:21:22 -0700

    Add create_json_schemas()

--- tap_brightview/helpers.py
+++ tap_brightview/helpers.py
@@ -1,3 +1,7 @@
+import tap_brightview.service as service
+from schema_builder import build_json_schema
+
+
 def create_table_list(tables_path):
     with open(f'{tables_path}') as tables:
         table_data = tables.readlines()
@@ -8,3 +12,17 @@ def create_table_list(tables_path):
         clean_data.append(row.rstrip())
 
     return clean_data
+
+
+def create_json_schemas():
+    table_list = create_table_list('db_tables.txt')
+    client = service.create_client()
+    sql = service.create_sql_cursor(client)
+
+    for table in table_list:
+        sql.execute(f"DESCRIBE FORMATTED brightview_prod.{table}")
+        query = sql.fetchall()
+
+        build_json_schema('table', data=query, table_name=table)
+
+    return 'JSON Schemas created successfully.'
