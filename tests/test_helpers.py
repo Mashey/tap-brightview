a11438509838a6c9e5268905b157631507d27364
Jordan <jordan.wallace.williams@gmail.com>
2021-01-28 20:13:21 -0700

    Update test_sql_to_json()

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -19,22 +19,10 @@ def test_sql_to_json():
         (1007, 'OIC', 'ODADAS Individual Counseling', '2010-01-01 00:00:00', '2015-01-02 00:00:00', 'C', None, 'Y', 'N', 'N', 'N', None, None, 15, 'Minutes', None, None, None, 1000, 1, '2010-12-03 08:19:37', 14884, '2016-01-26 13:02:35',
             'N', None, None, None, 1, None, None, None, None, 'N', 'N', None, None, None, 'MH', 'P', None, None, 'N', None, None, None, None, None, None, None, None, None, 'ins', '2020-11-09 21:44:16', '2020-11-11 00:01:27.165000')
     ]
-
-    with open('./tap_brightview/schemas/activity_schema.json') as schema:
-        json_schema = json.load(schema)
-
-    schema_keys = list(json_schema['properties'])
-    json_response = []
-
-    for row in response:
-        schema_properties = json_schema['properties']
-        key_value_pairs = list(zip(schema_keys, row))
-
-        for pair in key_value_pairs:
-            schema_properties[pair[0]] = pair[1]
-            json_response.append(schema_properties)
     
+    json_schema = helper.open_json_schema('activity')
+    json_response = helper.create_json_response(json_schema, response)
+
     assert isinstance(json_response, list)
     assert len(json_response) > 1
     assert isinstance(json_response[0], dict)
-
