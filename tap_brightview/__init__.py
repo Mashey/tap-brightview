c426ab6d6b25d5142b2f0ca812b6c81438cc9ab3
Ryan Allen <rcallen89@gmail.com>
2021-02-08 10:29:25 -0600

    Setup sync to take in collection

--- tap_brightview/__init__.py
+++ tap_brightview/__init__.py
@@ -2,6 +2,7 @@ import singer
 from singer.catalog import write_catalog
 from tap_brightview.discovery import discover
 from tap_brightview.sync import sync
+from tap_brightview.streams import STREAMS
 
 # Fill in any required config keys from the config.json here
 REQUIRED_CONFIG_KEYS = []
@@ -18,7 +19,7 @@ def main():
     if args.discover:
         write_catalog(catalog)
     else:
-        sync(args.config, args.state, catalog)
+        sync(args.config, args.state, catalog, stream_collection=STREAMS)
 
 
 if __name__ == '__main__':
