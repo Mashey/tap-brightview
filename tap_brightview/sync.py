1cebbc30337cfbe66aea165bad2925280dde9bb4
Ryan Allen <rcallen89@gmail.com>
2021-03-31 09:59:01 -0500

    Add metric tracking

--- tap_brightview/sync.py
+++ tap_brightview/sync.py
@@ -9,6 +9,9 @@ LOGGER = singer.get_logger()
 
 
 def sync(config, state, catalog, stream_collection):
+    total_records = []
+    stream_rps = []
+
     with Transformer() as transformer:
         for stream in catalog.get_selected_streams(state):
             start = time.perf_counter()
@@ -19,7 +22,7 @@ def sync(config, state, catalog, stream_collection):
             stream_schema = stream.schema.to_dict()
             stream_metadata = metadata.to_map(stream.metadata)
 
-            LOGGER.info(f'Staring sync for stream: {tap_stream_id}')
+            LOGGER.info(f"Staring sync for stream: {tap_stream_id}")
 
             singer.set_currently_syncing(state, tap_stream_id)
             singer.write_state(state)
@@ -27,38 +30,56 @@ def sync(config, state, catalog, stream_collection):
                 tap_stream_id,
                 stream_schema,
                 stream_obj.key_properties,
-                stream.replication_key
+                stream.replication_key,
             )
 
             for record in stream_obj.records_sync():
                 transformed_record = transformer.transform(
-                    record, stream_schema, stream_metadata)
+                    record, stream_schema, stream_metadata
+                )
 
-                # LOGGER.info(f"Writing record: {records_written}")
                 singer.write_record(
                     tap_stream_id,
                     transformed_record,
                 )
                 records_written += 1
 
-
             if records_written == 0:
-                LOGGER.info(
-                    f'No records found for {tap_stream_id}')
+                LOGGER.info(f"No records found for {tap_stream_id}")
             else:
-                stop = time.perf_counter()
-                elasped_time = stop - start
-                LOGGER.info(f'{tap_stream_id} completed in {elasped_time:0.4f} seconds. {elasped_time / records_written} rps')
-                LOGGER.info(
-                    f'Number of Records: {records_written}')
+                info, rps = metrics(
+                    start=start, end=time.perf_counter(), records=records_written
+                )
+                stream_rps.append(rps)
+                LOGGER.info(f"{info}")
+                singer.write_bookmark(state, tap_stream_id, "metrics", info)
+
                 singer.write_bookmark(
                     state,
                     tap_stream_id,
                     replication_key,
-                    record[stream_obj.replication_key]
+                    record[stream_obj.replication_key],
                 )
-                    
-
 
     state = singer.set_currently_syncing(state, None)
+    overall_rps = overall_metrics(total_records, stream_rps)
+    singer.write_bookmark(
+        state,
+        "Overall",
+        "metrics",
+        f"Records: {sum(total_records)} / RPS: {overall_rps:0.6}",
+    )
     singer.write_state(state)
+
+
+def metrics(start: float, end: float, records: int):
+    elapsed_time = end - start
+    rps = records / elapsed_time
+    info = f"Stream runtime: {elapsed_time:0.6} seconds / Records: {records} / RPS: {rps:0.6}"
+    return info, rps
+
+
+def overall_metrics(records: list, rps_list: list) -> float:
+    stream_count = len(records)
+    total_rps = sum(rps_list)
+    return total_rps / stream_count
